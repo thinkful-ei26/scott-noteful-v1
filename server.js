222e0e89c744f06670d386a2f0c37ab17461b1aa
Scott Ian <sim2487@gmail.com>
2018-11-13 12:43:32 -0800

    error module

--- server.js
+++ server.js
@@ -29,6 +29,24 @@ app.get('/api/notes/:id', (req, res) => {
 
 });
 
+// app.get('/boom', (req, res, next) => {
+//   throw new Error('Boom!!');
+// });
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
