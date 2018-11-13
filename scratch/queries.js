b07e73677976cc6d77f503ff35a5381735fb5dbe
Scott Ian <sim2487@gmail.com>
2018-11-13 13:22:36 -0800

    scratch queries file

---
+++ scratch/queries.js
@@ -0,0 +1,70 @@
+'use strict';
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+// GET Notes with search
+notes.filter('cats', (err, list) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(list);
+});
+
+// GET Notes by ID
+notes.find(1005, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+// PUT (Update) Notes by ID
+const updateObj = {
+  title: 'New Title',
+  content: 'Blah blah blah'
+};
+
+
+notes.update(1005, updateObj, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+const newObj = {
+  id: '11',
+  title: '11th title',
+  content: 'this is the content for the 11th title.',
+};
+
+notes.create(newObj, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+notes.delete('11', (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
